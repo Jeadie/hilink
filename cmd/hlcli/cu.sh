b505b0096e3156701ffa0dba2edfde6bdab9f793
jeadi <jeadi@amazon.com>
2022-08-10 11:10:54 +1000

    temp sh files

---
+++ cmd/hlcli/cu.sh
@@ -0,0 +1,8 @@
+curl 'http://192.168.8.1/api/user/login' --data '
+<request>
+	<Username>admin</Username>
+	<Password>admin</Password>
+</request>'
+curl 'http://192.168.8.1/api/cradle/mac'
+
+
---
+++ cmd/hlcli/test.sh
@@ -0,0 +1,5 @@
+curl 'http://192.168.8.1/api/webserver/SesTokInfo'
+
+
+
+
